2186542fe861e4e16785b1281d9f90786539da79
Mark Paul <mpaul730@gmail.com>
2019-09-28 01:03:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -34,14 +34,3 @@ def find_min_value(array)
   min
 end
 
-def find_element_index(array, value_to_find)
-  # Add your solution here
-end
-
-def find_max_value(array)
-  # Add your solution here
-end
-
-def find_min_value(array)
-  # Add your solution here
-end
